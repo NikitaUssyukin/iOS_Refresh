862a229f9f853af60848710e7d8b9ef93b41f3ff
Nikita <nikus0108@gmail.com>
2022-06-10 18:01:54 +0600

    sync commit

---
+++ StreamingServiceApp/StreamingServiceApp/Views/HeroHeaderUIView.swift
@@ -0,0 +1,70 @@
+//
+//  HeroHeaderUIView.swift
+//  StreamingServiceApp
+//
+//  Created by Yakuza on 10/06/2022.
+//
+
+import UIKit
+
+class HeroHeaderUIView: UIView {
+    
+    private let playButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Play", for: .normal)
+        button.layer.borderColor = UIColor.systemBackground.cgColor
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = .
+        //button.layer.cornerCurve =
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    private let heroImageView: UIImageView = {
+       
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleToFill
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "heroImage")
+        return imageView
+    }()
+    
+    private func addGradient() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [
+            UIColor.clear.cgColor,
+            UIColor.systemBackground.cgColor
+        ]
+        gradientLayer.frame = bounds
+        layer.addSublayer(gradientLayer)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(heroImageView)
+        addGradient()
+        addSubview(playButton)
+        applyConstraints()
+    }
+    
+    private func applyConstraints() {
+        
+        let playButtonConstraints = [
+            playButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 25),
+            playButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -20),
+            playButton.widthAnchor.constraint(equalToConstant: 100)
+        ]
+        
+        NSLayoutConstraint.activate(playButtonConstraints)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        heroImageView.frame = bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+
+}
