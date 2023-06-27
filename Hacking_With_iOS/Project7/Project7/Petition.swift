562b7daf177fb981473588f25ef6c0211f815f9e
NikitaUssyukin <nikita@Nikitas-MacBook-Pro.local>
2023-06-28 02:28:23 +0600

    Finished Project7

---
+++ Hacking_With_iOS/Project7/Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Nikita on 28.06.2023.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Hacking_With_iOS/Project7/Project7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Nikita on 28.06.2023.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
